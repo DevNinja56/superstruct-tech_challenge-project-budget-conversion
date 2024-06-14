92933886df29c0d536413f529ff3910b0d5c64dd
DevNinja56 <danisamim9@gmail.com>
2024-06-14 16:14:26 +0500

    test(mockData): add mock data for test cases

---
+++ test/mockData.js
@@ -0,0 +1,43 @@
+const MOCK_DATA = [
+  {
+    projectId: 1,
+    projectName: 'Humitas Hewlett Packard',
+    year: 2024,
+    currency: 'EUR',
+    initialBudgetLocal: 316974.5,
+    budgetUsd: 233724.23,
+    initialScheduleEstimateMonths: 13,
+    adjustedScheduleEstimateMonths: 12,
+    contingencyRate: 2.19,
+    escalationRate: 3.46,
+    finalBudgetUsd: 247106.75
+  },
+  {
+    projectId: 2,
+    projectName: 'Gallo en chicha Citi',
+    year: 2004,
+    currency: 'EUR',
+    initialBudgetLocal: 48736.11,
+    budgetUsd: 25228.62,
+    initialScheduleEstimateMonths: 15,
+    adjustedScheduleEstimateMonths: 14,
+    contingencyRate: 6.2,
+    escalationRate: 0.75,
+    finalBudgetUsd: 26993.74
+  },
+  {
+    projectId: 3,
+    projectName: 'Papadum Budweiser',
+    year: 2004,
+    currency: 'USD',
+    initialBudgetLocal: 522767.22,
+    budgetUsd: 358652.2,
+    initialScheduleEstimateMonths: 7,
+    adjustedScheduleEstimateMonths: 9,
+    contingencyRate: 7.98,
+    escalationRate: 3.68,
+    finalBudgetUsd: 401524.28
+  }
+]
+
+module.exports = MOCK_DATA
